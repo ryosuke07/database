1c6b4207997decc884999519de2438d2e974569c
ryosuke <51287509+ryosuke07@users.noreply.github.com>
2022-04-23 16:15:08 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,10 @@
-setTimeout("redirect()",10000);
-function redirect(){
+setTimeout("alert()",10000);
+
+function alert(){
+  'FANZAに遷移しますか？'<input type="submit" value="遷移する" >;
+  }
+
+onClick=function redirect(){
   location.href="https://www.dmm.co.jp";
   }
+  
